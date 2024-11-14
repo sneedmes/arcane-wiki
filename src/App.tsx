a4f66f6cbf2309acb59f707729ef4e4431e302a4
sneedmes <dallaqyan0610@gmail.com>
2024-11-14 23:52:45 +0400

    feat: created useState for header and added new component IntroSection.tsx with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Header from "./components/Header/Header";
+import {IntroSection} from "./components/IntroSection/IntroSection";
+import {Header} from "./components/Header/Header";
+import {CharactersSection} from "./components/CharactersSection/CharactersSection";
 
 function App() {
-  return (
-    <>
-      <Header/>
-    </>
-  );
+    const [tab, setTab] = useState('main')
+    return (
+        <>
+            <Header active={tab} onClick={setTab}/>
+            {tab === 'characters' && <CharactersSection/>}
+            {tab === 'main' && <IntroSection/>}
+        </>
+    );
 }
 
 export default App;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,37 @@
 import Button from "../Button/Button";
-import logo from "../../images/logo.png"
+import logo from "../../images/logo.png";
 import "./Header.css"
-import {useState} from "react";
 
-export default function Header(){
-    const [content, setContent] = useState('null')
-    const switchPageClick = (page: string)=>{
-        setContent(page)
-    }
+type TabSectionProps = {
+    active: string,
+    onClick: (tab: string) => void
+}
+
+export const Header = ({active, onClick}: TabSectionProps) => {
     return (
-        <header className="header">
-            <div className="logo">
-                <img src={logo} alt=""/>
-            </div>
-            <nav className="nav">
-                <Button text="CHARACTERS" isActive={content === 'characters'} handleClick={()=>switchPageClick('characters')}/>
-                <Button text="MEDIA" isActive={content === 'media'} handleClick={()=>switchPageClick('media')}/>
-                <Button text="MAIN" isActive={content === 'main'} handleClick={()=>switchPageClick('main')}/>
-                <Button text="TERRITORIES" isActive={content === 'territories'} handleClick={()=>switchPageClick('territories')}/>
-                <Button text="CONTACTS" isActive={content === 'contacts'} handleClick={()=>switchPageClick('contacts')}/>
-            </nav>
-        </header>
+        <>
+            <header className="header">
+                <div className="logo">
+                    <img src={logo} alt=""/>
+                </div>
+                <div className="nav">
+                    <Button isActive={active === 'characters'}
+                            handleClick={() => onClick('characters')}
+                            text='Characters'/>
+                    <Button isActive={active === 'media'}
+                            handleClick={() => onClick('media')}
+                            text='Media'/>
+                    <Button isActive={active === 'main'}
+                            handleClick={() => onClick('main')}
+                            text='Main'/>
+                    <Button isActive={active === 'territories'}
+                            handleClick={() => onClick('territories')}
+                            text='Territories'/>
+                    <Button isActive={active === 'contact'}
+                            handleClick={() => onClick('contact')}
+                            text='Contact'/>
+                </div>
+            </header>
+        </>
     )
 }
